b890fefb859d78b19ecf7bfd8e32b0bfc556b00f
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-07-02 22:44:31 +0000

    Complete Logic to rotate images

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,30 +1,48 @@
-  var search = function(query) {
+var index = 0;
+function rotateImage(images,stop){
+  $('#img_id').fadeOut('fast', function()
+  {
+    $(this).attr('src', images[index]);
+    $(this).fadeIn('fast', function()
+    {
+      if (index == images.length-1)
+      {
+        index = 0
+        clearInterval(stop);
+      }
+      else
+      {
+        index++;
+      }
+    });
+  });
+}
 
-  // var url = "https://api.instagram.com/v1/tags/search?q="+ query +"&access_token="+'<%= session[:access_token] %>'+"&callback=callbackFunction";
-  // $("#results").html("");
-
-  // var url = "https://api.instagram.com/v1/tags/"+ query +"/media/recent?access_token="+'<%= session[:access_token] %>'+"&callback=callbackFunction";
+  var search = function(query)
+  {
   var url = "https://api.instagram.com/v1/tags/"+ query +"/media/recent?access_token="+ access_token +"&callback=callbackFunction";
-
     $.ajax({
         url: url,
         dataType: "jsonp",
         success: function(index,response)
         {
+          var images = [];
           for(i=0;i<index.data.length;i++)
           {
-            // var img = $('<img id="dynamic">');
-            var img = $('#img_id');
-            img.attr('src', index.data[i].images.standard_resolution.url);
-            // img.appendTo('#id_div');
+            images.push(index.data[i].images.standard_resolution.url);
           }
+
+          alert(images.length);
+          var refreshIntervalId = setInterval(function () {
+          rotateImage(images,refreshIntervalId)
+            }, 1000);
         },
       });
   }
 
 // same as dom ready
 $(function(){
-  $("#submitbutton").click(function(ev) {
+  $("#submitform").submit(function(ev) {
   ev.preventDefault();
   var query = $('input[name="queryinput"]').val();
   search(query);
